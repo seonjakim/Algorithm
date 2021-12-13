812f517cdca0a453bf70899c9bf39a0134c762db
seonja <seonja.kim0@gmail.com>
2021-12-13 22:28:56 +0900

    graph

--- FROM21.12.04/undirectedGraph.js
+++ FROM21.12.04/undirectedGraph.js
@@ -83,3 +83,86 @@ const exploreSize = (graph, node, visited) => {
 }
 
 console.log(largestComponent(buildGraph(edges)))
+
+// shortest path
+
+const shortestPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges)
+  const visited = new Set([nodeA])
+  const queue = [[nodeA, 0]]
+
+  while (queue.length > 0) {
+    const [node, distance] = queue.shift()
+
+    if (node === nodeB) return distance
+    for (let neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor)
+        queue.push([neighbor, distance + 1])
+      }
+    }
+  }
+  return -1
+}
+
+// islandCount
+
+const islandCount = (grid) => {
+  const visited = new Set()
+  let count = 0
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      if (exploreIsland(grid, r, c, visited) === true) count++
+    }
+  }
+  return count
+}
+
+const exploreIsland = (grid, r, c, visited) => {
+  const rowInbounds = 0 <= r && r < grid.length
+  const colInbounds = 0 <= c && c < grid[0].length
+  if (!rowInbounds || !colInbounds) return false
+  if (grid[r][c] === 'W') return false
+  const pos = `${r},${c}`
+  if (visited.has(pos)) return false
+  visited.add(pos)
+
+  exploreIsland(grid, r - 1, c, visited)
+  exploreIsland(grid, r + 1, c, visited)
+  exploreIsland(grid, r, c - 1, visited)
+  exploreIsland(grid, r, c + 1, visited)
+  return true
+}
+
+// minimumIsland
+
+const minimumIsland = (grid) => {
+  const visited = new Set()
+  let minSize = Infinity
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      const size = exploreSize(grid, r, c, visited)
+      if (size > 0 && size < minSize) minSize = size
+    }
+  }
+  return minSize
+}
+
+const exploreSize = (grid, r, c, visited) => {
+  const rowInbounds = 0 <= r && r < grid.length
+  const colInbounds = 0 <= c && c < grid[0].length
+  if (!rowInbounds || !colInbounds) return 0
+
+  if (grid[r][c] === 'W') return 0
+
+  const pos = `${r},${c}`
+  if (visited.has(pos)) return 0
+  visited.add(pos)
+
+  let size = 1
+  size += exploreSize(grid, r - 1, c, visited)
+  size += exploreSize(grid, r + 1, c, visited)
+  size += exploreSize(grid, r, c - 1, visited)
+  size += exploreSize(grid, r, c + 1, visited)
+  return size
+}
